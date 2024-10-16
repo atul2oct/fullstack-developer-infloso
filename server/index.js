14decc7ea88b1792c45bb4018123858a93a80880
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2024-10-16 16:54:15 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = [
-    process.env.FRONE_END_URL_1 ,
-    process.env.FRONE_END_URL_2 ,
+    process.env.FRONE_END_URL_1 || 'https://fullstack-developer-infloso.vercel.app/',
+    process.env.FRONE_END_URL_2 || 'https://fullstack-developer-infloso-git-main-atuls-projects-ec02cbcd.vercel.app/',
 ];
 
 app.use(
