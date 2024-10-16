60b4e46278f939987492694bbc94132505ef373f
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2024-10-16 16:59:14 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = [
-    process.env.FRONE_END_URL_1 || 'https://fullstack-developer-infloso.vercel.app/',
-    process.env.FRONE_END_URL_2 || 'https://fullstack-developer-infloso-git-main-atuls-projects-ec02cbcd.vercel.app/',
+    'https://fullstack-developer-infloso.vercel.app/',
+    'https://fullstack-developer-infloso-git-main-atuls-projects-ec02cbcd.vercel.app/',
 ];
 
 app.use(
